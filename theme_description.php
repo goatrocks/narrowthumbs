b2836779a574777006114c52da4be2367c04144c
Meggle <meggani@gmail.com>
2011-09-07 22:06:30 -0400

    adding basic files for template

---
+++ theme_description.php
@@ -0,0 +1,8 @@
+<?php
+// Zenphoto theme definition file
+$theme_description['name'] = 'Narrow Thumbs';
+$theme_description['author'] = gettext('<a href="http://meggle.github.com" target="_blank">M Cahill</a> and <a href="http://www.anglatudisco.com" target="_blank">Angela Tudisco</a>');
+$theme_description['version'] = '0.1';
+$theme_description['date'] = '09/07/2011';
+$theme_description['desc'] = gettext("Narrow thumbnail theme.");
+?>
