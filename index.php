3343fe7979a6a772cf75f1965b7a57a419fe6f90
Meggle <meggani@gmail.com>
2011-09-10 17:03:16 -0400

    thumbnail mission accomplished
    Thumbnails don't look skewed, but need to confirm heigh & width w/Ang

--- index.php
+++ index.php
@@ -38,9 +38,10 @@ if (!defined('WEBPATH')) die();
 
 				<div class="span1 columns">&nbsp;</div> 
 				<div class="span1 columns">&nbsp;</div>
-				<div class="span4 columns"><a href="imagegallery.html"><img src="<?php echo $_zp_themeroot ?>/yella.png"><br/></a>GalleryTitle</div> 
-				<div class="span4 columns"><img src="<?php echo $_zp_themeroot ?>/yella.png"><br/>GalleryTitle</div> 
-				<div class="span4 columns"><img src="<?php echo $_zp_themeroot ?>/yella.png"><br/>GalleryTitle</div> 
+				<?php $i = 0; while(next_album() && $i < 3): ?>
+				<div class="span4 columns"><a href="<?php echo html_encode(getAlbumLinkURL());?>" title="<?php echo gettext('View album:'); ?> <?php echo getAnnotatedAlbumTitle();?>"> <?php printCustomAlbumThumbImage(getImageTitle(), NULL, 180, 400, 180, 400); ?></a></div> 
+				<?php $i++; ?> 
+				<?php endwhile; ?>
 				<div class="span1 columns">&nbsp;</div>
 				<div class="span1 columns">&nbsp;</div>
 				</div>
